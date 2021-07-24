fd576847913d1c774d11e534c9bd3eaffc6ea61e
瀬田秀太 <s.s_00883069@icloud.com>
2021-07-24 08:26:55 +0000

    first commit

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,14 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+});
+
+Route::get('XXX', 'AAAController@bbb');
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('admin/profile/create', 'ProfileController@add');
+    Route::get('admin/profile/edit', 'ProfileController@edit');
+});
