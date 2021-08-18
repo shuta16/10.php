fc574bfd1f342541c922ef406e18bdf89e0f12b3
瀬田秀太 <s.s_00883069@icloud.com>
2021-08-18 04:37:56 +0000

    laravel12

--- routes/web.php
+++ routes/web.php
@@ -15,18 +15,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'admin'], function() {
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
-});
-
-Route::get('XXX', 'AAAController@bbb');
-
-Route::group(['prefix' => 'admin'], function() {
+    
     Route::get('profile/create', 'Admin\ProfileController@add');
-    Route::post('profile/edit', 'Admin\ProfileController@edit');
+    Route::post('profile/create', 'Admin\ProfileController@create');
+    Route::get('profile/edit', 'Admin\ProfileController@edit');
+    Route::post('profile/edit', 'Admin\ProfileController@update');
 });
 
-Route::group(['prefix' => 'admin'], function() {
-    Route::post('profile/create', 'Admin\ProfileController@create');
-    Route::get('profile/edit', 'Admin\ProfileController@update');
-});
+//Route::get('XXX', 'AAAController@bbb');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
