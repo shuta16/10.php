b420cb955c781edb73927e37e302e84f3027fe74
瀬田秀太 <s.s_00883069@icloud.com>
2021-08-28 12:43:46 +0000

    laravel14

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Profile;
+
 class ProfileController extends Controller
 {
     //
@@ -13,8 +15,9 @@ class ProfileController extends Controller
         return view('admin.profile.create');
     }
     
-    public function create()
-    {
+    public function create(Request $request)
+    { 
+        $this->validate($request, Profile::$rules);
         return redirect('admin/profile/create');
     }
     
